fcaf0db243311155aa0cca639cb4560b3f01b748
IgorViV <iupgrader@mail.ru>
2022-06-24 00:14:22 +0300

    Fixed http client connection to the repository

--- backend/services/OmdbRepository.php
+++ backend/services/OmdbRepository.php
@@ -2,30 +2,31 @@
 
 namespace Wtw\backend\services;
 
-use GuzzleHttp\Client;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Client\ClientInterface;
 
 /**
  * Repository for getting information about a movie by its ID from an external service
  * using the Remote Repository interface
  *
- * @param string $apikey The user's api key for accessing the remote service
- * @param string $idData Movie ID
+ * @param string $apikey              The user's api key for accessing the remote service
+ * @param ClientInterface $httpClient Psr compatible http client
+ * @param string $idData              Movie ID
  */
 class OmdbRepository implements MoviesRepository
 {
     private string $apikey;
+    private ClientInterface $client;
 
-    public function __construct(string $apikey)
+    public function __construct(string $apikey, ClientInterface $httpClient)
     {
         $this->apikey = $apikey;
+        $this->client = $httpClient;
     }
 
     private function sendRequest(string $imdbId): ResponseInterface
     {
-        $client = new Client();
-
-        return $client->request('GET', 'http://www.omdbapi.com', [
+        return $this->client->request('GET', 'http://www.omdbapi.com', [
             'query' => [
                 'apikey' => $this->apikey,
                 'i' => $imdbId,
--- backend/services/Test.php
+++ backend/services/Test.php
@@ -1,10 +1,12 @@
 <?php
 
 use Wtw\backend\services\OmdbRepository;
+use GuzzleHttp\Client;
 
 require_once '../../vendor/autoload.php';
 
-$repository = new OmdbRepository('571da7ed');
+$client = new Client();
+$repository = new OmdbRepository('571da7ed', $client);
 $info = $repository->getData('tt0096895');
 
 echo '<pre>' . print_r($info, true) . '</pre>';
