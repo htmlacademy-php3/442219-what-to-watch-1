8f476fab65639ef6c28a692e7282c2ce6a5b5782
IgorViV <iupgrader@mail.ru>
2022-06-27 12:32:46 +0300

    Installs Laravel

---
+++ resources/views/start-wtw.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>WTW</title>
+</head>
+<body>
+    <h1>Hello, Laravel!</h1>
+    <p>Project "What to watch"</p>
+</body>
+</html>
