2aff978a36c13f0eea4064ef1891bfddcff7e470
IgorViV <iupgrader@mail.ru>
2022-08-02 10:36:22 +0300

    Prescribes relationships in models

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Favorite extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function film(): BelongsTo
+    {
+        return $this->belongsTo(Film::class);
+    }
 }
--- app/Models/Promo.php
+++ app/Models/Promo.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Promo extends Model
 {
     use HasFactory;
+
+    public function film(): BelongsTo
+    {
+        return $this->belongsTo(Film::class)->withDefault();
+    }
+
 }
